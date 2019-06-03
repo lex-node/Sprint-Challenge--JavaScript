5afb3eae3c36541e43f0981410a2521bfbc7082d
lex_node <gabriel.j.shapiro@gmail.com>
2019-06-02 22:02:04 -0400

    solves request 2 of advanced array methods portion of  objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -186,15 +186,15 @@ zooAnimals.forEach(animal => animalNames.push(`Name: ${animal.animal_name}, Scie
 console.log(animalNames);
 
 
+/* Request 2: .map()
 
-/* Request 2: .map()    
-
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the result.
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase());
 console.log(lowerCase);
+
 
 /* Request 3: .filter() 
 
