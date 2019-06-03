08ba69df7817e76248aafbf3aed2e90bb344015b
lex_node <gabriel.j.shapiro@gmail.com>
2019-06-02 22:03:42 -0400

    solves request 3 of advanced array methods portion of  objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -198,11 +198,13 @@ console.log(lowerCase);
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+const lowerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowerPopulation);
+
+
 
 /* Request 4: .reduce() 
 
